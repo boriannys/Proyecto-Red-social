146372cc2f987bbe42a3f50fed240f6d1bb31701
Boriannys <boridaza@gmail.com>
2018-04-21 14:29:16 -0430

    Red Social

---
+++ www/assets/js/loginController.js
@@ -0,0 +1,9 @@
+<script>
+angular.module('myApp', [])
+.controller('myCtrl', ['$scope', function($scope) {
+    $scope.count = 0;
+    $scope.myFunc = function() {
+        $scope.count++;
+    };
+}]);
+</script>
